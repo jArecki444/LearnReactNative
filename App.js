5fb29783a7a2f8bc6f83436b03d185144e8dc92c
jArecki444 <arkadiusz.cichurski@gmail.com>
2019-02-19 23:15:34 +0100

    Fetch user data with avatar images from API (ListComponent)

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import ListComponent from './app/components/ListComponent/ListComponent';
 
 export default class App extends React.Component {
--- app/components/ListComponent/ListComponent.js
+++ app/components/ListComponent/ListComponent.js
@@ -1,79 +1,65 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  ActivityIndicator,
-  Image
-} from "react-native";
-import { List } from "react-native-elements";
+import React, { Component } from "react";
+import { View, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
 
-export default class ListComponent extends React.Component {
+export default class ListComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { isLoading: true };
+
+    this.state = {
+      data: [],
+      error: null
+    };
   }
 
   componentDidMount() {
-    return fetch("https://jsonplaceholder.typicode.com/photos")
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState(
-          {
-            isLoading: false,
-            dataSource: responseJson
-          },
-          function() {}
-        );
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    this.makeRemoteRequest();
   }
 
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{ flex: 1, padding: 20 }}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
+  makeRemoteRequest = () => {
+    const url = `https://randomuser.me/api/?results=50`;
 
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: res.results,
+          error: res.error || null
+        });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  };
+
+  renderSeparator = () => {
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        <FlatList
-          data={this.state.dataSource}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <Text style={styles.row}>
-            Element id: {item.id} - Album id: {item.albumId}
-                <Image
-                    style={styles.elementImage}
-                    source={{ uri: item.thumbnailUrl}}
-                    />
-            </Text>
-          )}
-        />
-      </View>
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={({ item }) => (
+          <ListItem
+            title={`${item.name.first} ${item.name.last}`}
+            subtitle={item.email}
+            leftAvatar={{ source: { uri: item.picture.thumbnail } }}
+            containerStyle={{ borderBottomWidth: 0 }}
+          />
+        )}
+        keyExtractor={item => item.email}
+        ItemSeparatorComponent={this.renderSeparator}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f4f4f4',
-    marginBottom: 10
-  },
-  elementImage: {
-    width:150,
-    height:150,
-    marginBottom: 300,
-    backgroundColor: "#4286f4"
-  }
-});
